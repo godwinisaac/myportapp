77285da7097887f5525866dde565580fb39ec8b5
Devils Home <godwinnadar007@gmail.com>
2023-02-17 19:56:31 +0530

    responsiveness done

--- src/Components/features/Card.js
+++ src/Components/features/Card.js
@@ -4,7 +4,7 @@ import { HiArrowRight } from "react-icons/hi";
 const Card = ({title, des, icon}) => {
   return (
     <div className="w-full px-12 h-80 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-b from-bodyColor to-[#202327] group hover:bg-gradient-to-r hover:from-black hover:to-[#le2024] transition-colors duration-100 group">
-        <div className="h-72 overflow-y-hidden ">
+        <div className="h-100 overflow-y-hidden ">
             <div className="flex h-full flex-col gap-10 translate-y-16 group-hover:translate-y-0 transition-transform duration-500">
                 <div className="w-10 h-8 flex flex-col justify-between ">
                     {
--- src/Components/features/Features.js
+++ src/Components/features/Features.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SiAntdesign, SiServerless,SiWindows } from 'react-icons/si';
-import { FaGlobe, FaMobile } from "react-icons/fa";
+import { FaDatabase, FaMobile } from "react-icons/fa";
 
 import Title from '../Layouts/Title';
 import Card from './Card';
@@ -12,32 +12,32 @@ const Features = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         <Card
          title="Web App Develoment"
-         des="Lorem ipsum dolor sit amet consectrtur adipisicing elit. Atque soluta hic consequntur eum repellendus ad"
+         des="A web application is an application program that is stored on a remote server and delivered over the internet through a browser interface."
          
         />
         <Card
          title="Window App Develoment"
-         des="Lorem ipsum dolor sit amet consectrtur adipisicing elit. Atque soluta hic consequntur eum repellendus ad"
+         des="Creating and implementing the source code of new applications. Testing source code and debugging code."
          icon={<SiWindows />}
         />
         <Card
          title="Mobile web Develoment"
-         des="Lorem ipsum dolor sit amet consectrtur adipisicing elit. Atque soluta hic consequntur eum repellendus ad"
+         des="A mobile app developer uses programming languages and development skills to create, test, & develop app on mobile devices."
          icon={<FaMobile />}
         />
         <Card
          title="UX Design"
-         des="Lorem ipsum dolor sit amet consectrtur adipisicing elit. Atque soluta hic consequntur eum repellendus ad"
+         des="UI UX designers create the user interface for an app, website, or other interactive media."
          icon={<SiAntdesign />}
         />
         <Card
-         title="Hosting Websites"
-         des="Lorem ipsum dolor sit amet consectrtur adipisicing elit. Atque soluta hic consequntur eum repellendus ad"
-         icon={<FaGlobe />}
+         title="Data-Base"
+         des="A Database Developer, or Database Programmer, is responsible for creating, administering and troubleshooting computer databases that can process large amounts of information and keep it secure."
+         icon={<FaDatabase />}
         />
         <Card
-         title="Backend Develoment"
-         des="Lorem ipsum dolor sit amet consectrtur adipisicing elit. Atque soluta hic consequntur eum repellendus ad"
+         title="Back-end Develoment"
+         des="Compile and analyze data, processes, and codes to troubleshoot problems and identify areas for improvement."
          icon={<SiServerless />}
         />
       </div>
